4d2dbf3711bc671ac0338f51915378eaadea0184
SqLait <deanroman2005@gmail.com>
2024-07-17 09:32:28 +0200

    Added: base for config checking
    basic sample of checking if a config file exists. And if not it will
    create one.

--- config/CheckConfigFile.c
+++ config/CheckConfigFile.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int ExistsInConfig(const char *fname) {
   FILE *file;
@@ -14,4 +16,37 @@ int ExistsInConfig(const char *fname) {
 }
 
 void ApplyConfig() {
+
+}
+
+int StartConfig() {
+  if (!ExistsInConfig("config.c") || !ExistsInConfig("config.lua")) {
+    printf("No config file detected in ~/.config/plush.\n\nWould you like to generate one? [y/n]: ");
+    char opt = getchar();
+    if (opt == 'y') {
+      if (mkdir("~/.config/plush", 700) != 0) {
+        return 1;
+      }
+
+      FILE *file = fopen("config.lua", "w");
+      if (file == NULL) {
+        perror("fopen failed");
+        return 1;
+      }
+      return 0;
+
+    } else if (opt == 'n'){
+      printf("\nConfig not created. Continue with built-in config.");
+      return 1;
+
+    } else {
+      printf("\nDefaulted to N.");
+      return 1;
+    }
+  } else if (ExistsInConfig("config.c") || ExistsInConfig("config.lua")) {
+    ApplyConfig();
+  } else {
+    perror("Config related issue. Can't find or init the file.");
+  }
+  return 0;
 }
