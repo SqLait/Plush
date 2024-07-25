fb36499699578a3d7eb3eb9c38956f300fb0e367
SqLait <deanroman2005@gmail.com>
2024-07-25 11:00:13 +0200

    Added: building and compiling with cmake
    This makes a lot of things easier for me to test during work

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/config/ApplyInit.c.o"
+  "CMakeFiles/main.dir/src/config/ApplyInit.c.o.d"
+  "CMakeFiles/main.dir/src/config/CheckConfigFile.c.o"
+  "CMakeFiles/main.dir/src/config/CheckConfigFile.c.o.d"
+  "CMakeFiles/main.dir/src/config/ExistsInConfig.c.o"
+  "CMakeFiles/main.dir/src/config/ExistsInConfig.c.o.d"
+  "CMakeFiles/main.dir/src/history/History.c.o"
+  "CMakeFiles/main.dir/src/history/History.c.o.d"
+  "CMakeFiles/main.dir/src/main.c.o"
+  "CMakeFiles/main.dir/src/main.c.o.d"
+  "CMakeFiles/main.dir/src/shell.c.o"
+  "CMakeFiles/main.dir/src/shell.c.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
